b2fd79cdf56b3f0f2249282551289b27f496e1b0
gooma519 <gooma519@gmail.com>
2026-02-13 01:02:48 +0900

    learn: Up to lecture31

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,22 +1,20 @@
 import classes from './NewPost.module.css';
-import {useState} from "react";
 
-function NewPost() {
-    const [enteredBody, setEnteredBody] = useState('')
-    const changeBodyHandler = (e) => {
-        setEnteredBody(e.target.value)
-    }
+function NewPost({onChangeBody, onChangeAuthor, onClose}) {
   return (
     <form className={classes.form}>
-      <p>
-        <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
-      </p>
-        <p>{enteredBody}</p>
-      <p>
-        <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
-      </p>
+        <p>
+            <label htmlFor="body">Text</label>
+            <textarea id="body" required rows={3} onChange={onChangeBody}/>
+            </p>
+            <p>
+            <label htmlFor="name">Your name</label>
+            <input type="text" id="name" required onChange={onChangeAuthor}/>
+        </p>
+        <p className={classes.actions}>
+            <button type="button" onClick={onClose}>Cancel</button>
+            <button type="submit">Submit</button>
+        </p>
     </form>
   );
 }
