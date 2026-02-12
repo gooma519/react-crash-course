f59b710f1a15fab6d30b90baa792768c2399dd73
gooma519 <gooma519@gmail.com>
2026-02-13 01:02:48 +0900

    ì¡git error?

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import './App.css'
 import PostList from "./components/PostList.jsx";
+import MainHeader from "./components/MainHeader.jsx";
+import {useState} from "react";
 
 function App() {
+  const [modalVisible, setModalVisible] = useState(true)
+
+  const showModalHandler = () => {
+    setModalVisible(true)
+  }
+
+  const hideModalHandler = () => {
+    setModalVisible(false)
+  }
 
   return (
-    <div><PostList /></div>
+      <>
+        <MainHeader onCreatePost={showModalHandler}/>
+        <main>
+          <PostList onClose={hideModalHandler} modalVisible={modalVisible}/>
+        </main>
+      </>
   )
 }
 
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Post from "./Post.jsx";
 import classes from "./PostList.module.css"
 import NewPost from "./NewPost.jsx";
+import Modal from "./Modal.jsx";
 
 
-function PostList() {
+function PostList({onClose, modalVisible}) {
+    const [enteredBody, setEnteredBody] = useState('')
+    const [author, setAuthor] = useState('')
+    const changeBodyHandler = (e) => {
+        setEnteredBody(e.target.value)
+    }
+    const changeAuthorHandler = (e) => {
+        setAuthor(e.target.value)
+    }
+
     return (
         <>
-            <NewPost />
-            <list className={classes.posts}>
-                <Post author="max" body="hi"/>
-                <Post author="mag" body="kill"/>
-            </list>
+            {modalVisible &&
+                <Modal onClose={onClose}>
+                    <NewPost
+                    onChangeBody={changeBodyHandler}
+                    onChangeAuthor={changeAuthorHandler}
+                    onClose={onClose}
+                    />
+                </Modal>
+            }
+            <ul className={classes.posts}>
+                <Post author="max" body={enteredBody}/>
+                <Post author="mag" body={author}/>
+            </ul>
         </>
     );
 }
